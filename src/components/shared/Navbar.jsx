b7c1f00e7a91e30c580fa02cbf90176df8ae5191
Mijanur Rahman <mijanurrahman0645@gmail.com>
2026-04-17 16:16:59 +0600

    make navbar responsive for all device

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -6,31 +6,105 @@ import MyNavLink from "./MyNavLink";
 
 const Navbar = () => {
   return (
-    <nav className="shadow">
-      <div className="flex justify-between p-4  container mx-auto">
-        <img src={logoImg} alt="" />
-        <ul className="flex gap-2">
-          <li>
-            <MyNavLink to={"/"}>
-              <House />
-              Home
-            </MyNavLink>
-          </li>
-          <li>
-            <MyNavLink to={"/timeline"}>
-              <Clock3 />
-              Timeline
-            </MyNavLink>
-          </li>
-          <li>
-            <MyNavLink to={"/stats"}>
-              <ChartLine />
-              Stats
-            </MyNavLink>
-          </li>
-        </ul>
+    <>
+      <nav className=" bg-base-100 shadow-sm">
+        <div className="navbar container mx-auto">
+        <div className="navbar p-1 container mx-auto">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+              >
+              <li>
+              <MyNavLink to={"/"}>
+                <House />
+                Home
+              </MyNavLink>
+            </li>
+            <li>
+              <MyNavLink to={"/timeline"}>
+                <Clock3 />
+                Timeline
+              </MyNavLink>
+            </li>
+            <li>
+              <MyNavLink to={"/stats"}>
+                <ChartLine />
+                Stats
+              </MyNavLink>
+            </li>
+            </ul>
+          </div>
+          <img src={logoImg} alt="" />
+        </div>
+        <div className="navbar hidden lg:flex justify-end">
+          <ul className="menu menu-horizontal px-1 gap-2">
+            <li>
+              <MyNavLink to={"/"}>
+                <House />
+                Home
+              </MyNavLink>
+            </li>
+            <li>
+              <MyNavLink to={"/timeline"}>
+                <Clock3 />
+                Timeline
+              </MyNavLink>
+            </li>
+            <li>
+              <MyNavLink to={"/stats"}>
+                <ChartLine />
+                Stats
+              </MyNavLink>
+            </li>
+          </ul>
+        </div>
       </div>
-    </nav>
+      </nav>
+{/* 
+      <nav className="shadow">
+        <div className="flex justify-between p-4  container mx-auto">
+          <img src={logoImg} alt="" />
+          <ul className="flex gap-2">
+            <li>
+              <MyNavLink to={"/"}>
+              <House />
+                Home
+                </MyNavLink>
+            </li>
+            <li>
+              <MyNavLink to={"/timeline"}>
+                <Clock3 />
+                Timeline
+              </MyNavLink>
+            </li>
+            <li>
+              <MyNavLink to={"/stats"}>
+                <ChartLine />
+                Stats
+              </MyNavLink>
+            </li>
+          </ul>
+        </div>
+      </nav> */}
+    </>
   );
 };
 
